6c328ef5896b82014b8c6e09b93e67e44779c9dc
guoyoujin <1132576362@qq.com>
2015-03-17 15:12:44 +0800

    非规范化评论数量到帖子中

--- server/fixtures.js
+++ server/fixtures.js
@@ -28,7 +28,7 @@ if (Posts.find().count() === 0) {
 
 
 
-// Fixture data
+// 测试数据 
 if (Posts.find().count() === 0) {
   var now = new Date().getTime();
 
@@ -47,7 +47,8 @@ if (Posts.find().count() === 0) {
     userId: sacha._id,
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
-    submitted: new Date(now - 7 * 3600 * 1000)
+    submitted: new Date(now - 7 * 3600 * 1000),
+    commentsCount: 2
   });
 
   Comments.insert({
@@ -71,7 +72,8 @@ if (Posts.find().count() === 0) {
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://meteor.com',
-    submitted: new Date(now - 10 * 3600 * 1000)
+    submitted: new Date(now - 10 * 3600 * 1000),
+    commentsCount: 0
   });
 
   Posts.insert({
@@ -79,6 +81,7 @@ if (Posts.find().count() === 0) {
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
-    submitted: new Date(now - 12 * 3600 * 1000)
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0
   });
 }
